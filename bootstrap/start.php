55f6e6088eeb7d078602a3ab34254eac8533ab1b
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-04-27 16:22:01 +0200

    fix env

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -27,7 +27,7 @@ $app = new Illuminate\Foundation\Application;
 $env = $app->detectEnvironment(array(
 
 	'local' => array('Pornografia'),
-    'production' => array('mlawnik'),
+    'working' => array('mlawnik'),
 
 ));
 
