300e0b812c361fe02dd5f135349f55061cef7ec2
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-04-27 16:29:47 +0200

    add gitignore

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => '',
-			'username'  => '',
-			'password'  => '',
+			'database'  => $_ENV['mysql_database'],
+			'username'  => $_ENV['mysql_username'],
+			'password'  => $_ENV['mysql_password'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
