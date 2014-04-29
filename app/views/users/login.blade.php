1299bcd30423582311c58b35df4e39eb83bc9442
Robsonwp <perzfilip@gmail.com>
2014-04-29 17:48:34 +0200

    delete php

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,11 +1,10 @@
 
-
     <div class="container">
         <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
             <div class="panel panel-primary" >
                     <div class="panel-heading">
                         <div class="panel-title">Zaloguj się</div>
-                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="password_reset.php" style="color:#FFF;"><?php echo WORDING_FORGOT_MY_PASSWORD; ?></a></div>
+                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="password_reset.php" style="color:#FFF;"></a></div>
                     </div>
    
 @if(Session::has('message'))
