16624fcfebfc2a0a137953c321e9c5d6a93c4fce
Robsonwp <perzfilip@gmail.com>
2014-04-29 17:43:49 +0200

    podpinanie wyglądu

--- app/views/users/login.blade.php
+++ app/views/users/login.blade.php
@@ -1,11 +1,4 @@
-{{ Form::open(array('url'=>'users/signin', 'class'=>'form-signin')) }}
-<h2 class="form-signin-heading">Please Login</h2>
 
-{{ Form::text('email', null, array('class'=>'input-block-level', 'placeholder'=>'Email Address')) }}
-{{ Form::password('password', array('class'=>'input-block-level', 'placeholder'=>'Password')) }}
-
-{{ Form::submit('Login', array('class'=>'btn btn-large btn-primary btn-block'))}}
-{{ Form::close() }}
 
     <div class="container">
         <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
@@ -14,26 +7,12 @@
                         <div class="panel-title">Zaloguj się</div>
                         <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="password_reset.php" style="color:#FFF;"><?php echo WORDING_FORGOT_MY_PASSWORD; ?></a></div>
                     </div>
-                    <?php
-// show potential errors / feedback (from login object)
-if (isset($login)) {
-    if ($login->errors) {
-        foreach ($login->errors as $error) {
-            echo '<div id="login-alert" class="alert alert-danger col-sm-12" style="padding:10px;">'.$error.'</div>';
-        }
-    }
-    if ($login->messages) {
-        foreach ($login->messages as $message) {
-            echo '<div id="login-alert" class="alert alert-success col-sm-12 style="padding:10px;"">'.$message.'</div>';
-        }
-    }
-}
-?>
+   
 @if(Session::has('message'))
-<p class="alert">{{ Session::get('message') }}</p>
+<p class="alert alert-danger col-sm-12">{{ Session::get('message') }}</p>
 @endif
 @if(Session::has('error'))
-<p class="alert">{{ Session::get('error') }}</p>
+<p class="alert alert-success col-sm-12">{{ Session::get('error') }}</p>
 @endif
                     <div style="padding-top:30px" class="panel-body" >
                         {{ Form::open(array('url'=>'users/signin', 'class'=>'form-horizontal')) }}
