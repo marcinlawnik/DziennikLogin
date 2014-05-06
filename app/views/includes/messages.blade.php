921522efef42899df08bcd5bafc983656d7e2b58
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-05-06 16:07:03 +0200

    Add margin to messages

--- app/views/includes/messages.blade.php
+++ app/views/includes/messages.blade.php
@@ -1,6 +1,6 @@
 @if(Session::has('message'))
-<p class="alert alert-danger col-sm-12" style="margin: 5px;">{{ Session::get('message') }}</p>
+<p class="alert alert-danger col-sm-10">{{ Session::get('message') }}</p>
 @endif
 @if(Session::has('error'))
-<p class="alert alert-success col-sm-12" style="margin: 5px;">{{ Session::get('error') }}</p>
+<p class="alert alert-success col-sm-10">{{ Session::get('error') }}</p>
 @endif
