30350a53ac902d13bb27099c7ab269afaf349763
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-05-08 19:04:23 +0200

    Remove ardent references

--- app/models/Grade.php
+++ app/models/Grade.php
@@ -5,22 +5,6 @@ class Grade extends Eloquent {
 
     protected $guarded = array('id');
 
-    /**
-     * Ardent validation rules
-     */
-    public static $rules = array(
-        'user_id' => 'numeric',
-        'subject_id' => 'numeric',
-        'grade_value' => '',
-        'grade_weight' => 'numeric',
-        'grade_group' => '',
-        'grade_title' => '',
-        'grade_date' => '',
-        'grade_abbreviation' => '',
-        'grade_trimester' => '',
-        'grade_download_date' => '',
-    );
-
     public function subject()
     {
         return $this->belongsTo('Subject');
--- app/models/Subject.php
+++ app/models/Subject.php
@@ -5,14 +5,6 @@ class Subject extends Eloquent {
 
     protected $guarded = array('id');
 
-    /**
-     * Ardent validation rules
-     */
-    public static $rules = array(
-        'id' => 'numeric',
-        'name' => '',
-    );
-
     public function grades()
     {
         return $this->hasMany('Grade');
