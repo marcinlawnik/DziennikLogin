f55087eea032704ee52724708dfc6c76c8b55bf4
qwewq <wojtek.koszela@interia.pl>
2014-05-13 17:29:41 +0200

    Update GradesController.php

--- app/controllers/GradesController.php
+++ app/controllers/GradesController.php
@@ -8,7 +8,8 @@ class GradesController extends \BaseController {
         // Show all grades belonging to user
         $grades = User::find(Auth::user()->id)->grades;
         if(!empty($grades)){
-            return View::make('grades.index')->withGrades($grades);
+            $average=Subject::calculateAverage();
+            return View::make('grades.index')->withGrades($grades)->withAverage($average);
         } else {
             return View::make('grades.index')->with('message', 'Brak ocen!')->withGrades('');
         }
