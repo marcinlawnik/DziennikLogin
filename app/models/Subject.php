cec572bd540cbaa39a85006559d589adddb6a36f
qwewq <wojtek.koszela@interia.pl>
2014-05-13 17:25:08 +0200

    Update Subject.php

--- app/models/Subject.php
+++ app/models/Subject.php
@@ -9,6 +9,21 @@ class Subject extends Eloquent {
     {
         return $this->hasMany('Grade');
     }
+    
+    public function calculateAverage() {
+        $grades = Grade::all()->where('user_id', '=', Auth::user()->id);
+        
+        $sumaOcen=0;
+        $sumaWag=0;
+        
+         foreach($grades as $key => $value) {
+             $sumaOcen += $value->value * $value->weight;
+             $sumaWag += $value->weight;
+         }
+         
+         return $sumaOcen/$sumaWag;
+         
+    }
 
     //TODO: Create a many-to-many relationship between users and subjects
 }
