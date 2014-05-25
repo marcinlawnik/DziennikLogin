6a2effe6ed7d03f31492b35689d4d84eaaaaf882
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-05-25 21:17:37 +0200

    Changed email template

--- app/views/emails/grades.blade.php
+++ app/views/emails/grades.blade.php
@@ -1,19 +1,29 @@
-Witaj,
+<html>
+<head>
 
-Poniżej znajduja się twoje oceny.
+</head>
+<body>
+<p>
+Witaj,<br>
+
+Poniżej znajduja się twoje oceny.<br>
 Oceny uszeregowane są od najwyższej do najniższej.
-
+</p><br>
+<p>
 @foreach($grades as $grade)
 Data: {{ $grade->date  }}<br>
 Ocena: {{ $grade->value  }}<br>
 Waga: {{ $grade->weight  }}<br>
 Przedmiot: {{ $grade->subject_id  }}<br>
 Trymestr: {{ $grade->trimester  }}<br>
-Tytuł: {{ $grade->title  }}<br>
+Tytuł: {{ $grade->title  }}<br><br>
 
 @endforeach
-
-
-Z poważaniem,
+</p>
+<p>
+Z poważaniem,<br>
 DziennikLogin
+</p>
+</body>
+</html>
 
