ba8ddab6285d183d6e82b5e0e2766bf284cc1d01
qwewq <wojtek.koszela@interia.pl>
2014-06-03 16:57:21 +0200

    Update ideas.pl

--- ideas.pl
+++ ideas.pl
@@ -3,7 +3,7 @@ Co ma sie znajdowac na str.głównej:
 -wyloguj
 -Oceny i filtry do nnich
 -link do strony glownej
--zmiana hasła
+-zmiana hasła zrobione
 -wykresy  z ocenami
 
 Dotatki- liczenie średniej końcowej - kalkulator
