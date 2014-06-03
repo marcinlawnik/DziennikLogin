1dfb813d5d1a5ac28a9d27691d60b5b8e94eaf35
qwewq <wojtek.koszela@interia.pl>
2014-06-03 17:06:50 +0200

    Update index.blade.php

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -1,7 +1,48 @@
 @include('dashboard.header')
 
 {{ $content }}
+<div class="container">
+<div class="row">
+<div class="col-sm-12 col-md-12">
+    <div class="well">
+        <table class="table table-striped" id="table_grades">
+            <thead>
+            <tr>
+                <th>Ocena</th>
 
-{{ var_dump($other) }}
+                <th>Waga</th>
 
+                <th>Grupa</th>
+
+                <th>Tytuł</th>
+
+                <th>Skrót</th>
+
+                <th>Data</th>
+
+                <th>Przedmiot</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            @foreach($content as $key => $value)
+            <tr>
+                <td><span>{{ $value->value }}</span></td>
+                <td><span>{{ $value->weight }}</span></td>
+                <td><span>{{ $value->group }}</span></td>
+                <td><span>{{ $value->title }}</span></td>
+                <td><span>{{ $value->abbreviation }}</span></td>
+                <td><span>{{ $value->date }}</span></td>
+                <td><span>{{ $value->subject->name  }}</span></td>
+                <td>
+                    <span><a class="btn btn-small btn-success" href="{{ URL::to('grades/show/' . $value->id) }}">Szczegóły</a>
+                </td>
+            </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+</div>
+</div>
 @include('includes.footer')
