c478bc04458d6954b4fa3ee90c6fe6d10947ec4e
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-06-05 22:52:33 +0200

    Update disclaimer

--- app/views/includes/disclaimer.blade.php
+++ app/views/includes/disclaimer.blade.php
@@ -1,5 +1,7 @@
-<p class="alert alert-info col-sm-12">DziennikLogin obecnie znajduje się w fazie testów.<br />
-    Cały system i wszystkie jego podzespoły są udostępniane na zasadach "tak jak jest" i "tak jak jest dostępne". <br />
-    DziennikLogin nie ponosi odpowiedzialności za jakiekolwiek straty.<br/>
+<p class="alert alert-info col-sm-12">
+    DziennikLogin obecnie znajduje się w fazie testów.<br />
+    Cały system i wszystkie jego podzespoły są udostępniane na zasadach "tak jak jest" i "tak jak będzie dostępne". <br />
+    DziennikLogin nie ponosi odpowiedzialności za jakiekolwiek straty.<br />
+    Korzystaj rozsądnie.<br />
     Błędy, opinie i sugestie kieruj na marcin [małpa] lawniczak.me
 </p>
