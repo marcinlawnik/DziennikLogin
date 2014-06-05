f5b16b4f7fa0b6f2b6abd3a67ab0600db1b87aee
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-06-05 22:55:58 +0200

    Update styling of disclaimer

--- app/views/includes/disclaimer.blade.php
+++ app/views/includes/disclaimer.blade.php
@@ -1,4 +1,4 @@
-<p class="alert alert-info col-sm-12">
+<p class="alert alert-info col-sm-12" style="margin-left:2%;margin-right:2%; width:96%">
     DziennikLogin obecnie znajduje się w fazie testów.<br />
     Cały system i wszystkie jego podzespoły są udostępniane na zasadach "tak jak jest" i "tak jak będzie dostępne". <br />
     DziennikLogin nie ponosi odpowiedzialności za jakiekolwiek straty.<br />
