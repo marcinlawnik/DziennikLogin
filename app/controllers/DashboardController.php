0462423b76ea6b7e79bb5b0e964de98832dce94f
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-06-28 19:31:11 +0200

    Removed old code

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -16,13 +16,6 @@ class DashboardController extends \BaseController {
             Session::flash('message', 'Nie posiadasz żadnych ocen! Kliknij '.link_to('firejob', 'tutaj').', aby uruchomić proces pobierania.');
         }
 
-//        $job_is_active = 1;
-//        $content = User::with('Settings')->whereHas('Settings',
-//                function($query) use ($job_is_active) {
-//                    $query->where('job_is_active', '=', $job_is_active);
-//                })
-//                ->get();
-
         $content = Grade::where('user_id', '=', Sentry::getUser()->id)->orderBy('date', 'DESC')->take(10)->get();
           
 
