8f7740cc7730319c42ae49229f1329f3de598a69
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-07-30 22:13:44 +0200

    Updated seeders for density of data

---
+++ app/database/seeds/SubjectsDemoSeeder.php
@@ -0,0 +1,116 @@
+<?php
+
+class SubjectsDemoSeeder extends Seeder {
+
+    public function run()
+    {
+        //DB::table('users')->delete();
+
+        $subjects = array(
+            array(
+                'id' => '1',
+                'name' => 'biologia',
+            ),
+            array(
+                'id' => '2',
+                'name' => 'chemia',
+            ),
+            array(
+                'id' => '3',
+                'name' => 'fizyka',
+            ),
+            array(
+                'id' => '4',
+                'name' => 'geografia',
+            ),
+            array(
+                'id' => '5',
+                'name' => 'historia',
+            ),
+            array(
+                'id' => '6',
+                'name' => 'j. niemiecki',
+            ),
+            array(
+                'id' => '7',
+                'name' => 'j. polski',
+            ),
+            array(
+                'id' => '8',
+                'name' => 'matematyka',
+            ),
+            array(
+                'id' => '9',
+                'name' => 'plastyka',
+            ),
+            array(
+                'id' => '10',
+                'name' => 'wos',
+            ),
+            array(
+                'id' => '11',
+                'name' => 'wf',
+            ),
+            array(
+                'id' => '12',
+                'name' => 'j. angielski z',
+            ),
+            array(
+                'id' => '13',
+                'name' => 'religia',
+            ),
+            array(
+                'id' => '14',
+                'name' => 'lekcja wychowawcza',
+            ),
+            array(
+                'id' => '15',
+                'name' => 'wn biomedycyna',
+            ),
+            array(
+                'id' => '16',
+                'name' => 'informatyka',
+            ),
+            array(
+                'id' => '17',
+                'name' => 'j. angielski k',
+            ),
+            array(
+                'id' => '18',
+                'name' => 'wi',
+            ),
+            array(
+                'id' => '19',
+                'name' => 'edukacja dla bezpieczeństwa',
+            ),
+            array(
+                'id' => '20',
+                'name' => 'chemia pz',
+            ),
+            array(
+                'id' => '21',
+                'name' => 'fizyka pz',
+            ),
+            array(
+                'id' => '22',
+                'name' => 'j. niemiecki pz',
+            ),
+            array(
+                'id' => '23',
+                'name' => 'matematyka pz',
+            ),
+            array(
+                'id' => '24',
+                'name' => 'wn informatyka',
+            ),
+
+        );
+
+
+        foreach ($subjects as $subject) {
+            Subject::create($subject);
+        }
+
+    }
+
+}
