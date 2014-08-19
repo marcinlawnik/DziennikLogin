dacd57df61a8464b956a8bc8442483f35d6f0387
Marcin Ławniczak <marcin.safmb@wp.pl>
2014-08-19 09:29:04 +0200

    Poprawka, jeżeli snapshot pusty

--- app/views/dashboard/index.blade.php
+++ app/views/dashboard/index.blade.php
@@ -25,17 +25,19 @@
             </thead>
 
             <tbody>
-            @foreach($content as $key => $value)
-            <tr>
-                <td><span>{{ $value->value }}</span></td>
-                <td><span>{{ $value->weight }}</span></td>
-                <td><span>{{ $value->group }}</span></td>
-                <td><span>{{ $value->title }}</span></td>
-                <td><span>{{ $value->abbreviation }}</span></td>
-                <td><span>{{ $value->date }}</span></td>
-                <td><span>{{ $value->subject->name  }}</span></td>
-            </tr>
-            @endforeach
+            @if(count($content) >= 1)
+                @foreach($content as $key => $value)
+                <tr>
+                    <td><span>{{ $value->value }}</span></td>
+                    <td><span>{{ $value->weight }}</span></td>
+                    <td><span>{{ $value->group }}</span></td>
+                    <td><span>{{ $value->title }}</span></td>
+                    <td><span>{{ $value->abbreviation }}</span></td>
+                    <td><span>{{ $value->date }}</span></td>
+                    <td><span>{{ $value->subject->name  }}</span></td>
+                </tr>
+                @endforeach
+            @endif
             </tbody>
         </table>
     </div>
